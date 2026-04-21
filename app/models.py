760eb6236da297dd05be703846e2be24368f5858
mdkawsar <info.mdkawsardev@gmail.com>
2026-04-21 20:18:31 +0600

    Get and Post method have been applied

--- app/models.py
+++ app/models.py
@@ -5,3 +5,10 @@ class userData(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField(default=0)
     nationality = models.CharField(max_length=100, default='Bangladeshi')
+
+class Students(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    section = models.CharField(max_length=100)
+    blood_group = models.CharField(max_length=100)
+    roll = models.IntegerField()
--- app/views.py
+++ app/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Students
+from .serializers import StudentSerializer
+from rest_framework import status
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+# @api_view(['GET'])
+# def students(request):
+#     details = {
+#         'Name': 'Kawsar',
+#         'Age': 23,
+#         'Nationality': 'Bangladeshi'
+#     }
+#     return Response(details)
+
+@api_view(['GET', 'POST'])
+def students(request):
+    if request.method == 'GET':
+        obj = Students.objects.all()
+        serializer = StudentSerializer(obj, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
