6112cdabd3114a4185372631f8216b36edd27ca2
mdkawsar <info.mdkawsardev@gmail.com>
2026-04-22 22:02:10 +0600

    Added validation of data

--- app/serializers.py
+++ app/serializers.py
@@ -7,3 +7,10 @@ class StudentSerializer(serializers.ModelSerializer):
         model = Students
         # exclude = ['id', 'name'] Displays all the data excerpt id and name
         fields = '__all__' # Displays all the data
+    def validate(self, data):
+        if data['age'] < 18:
+            raise serializers.ValidationError('Age should be above of 18')
+        specialchars = "!@#$%^&*()-=<>?/:'\n"
+        if any(a in specialchars for a in data['name']):
+            raise serializers.ValidationError('Special characters are not allowed in name!')
+        return data
