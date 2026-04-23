8009735011f50aa71eb131ad58ed7f71080904e6
mdkawsar <info.mdkawsardev@gmail.com>
2026-04-23 10:51:12 +0600

    Added a new comment about 'depth' or 'serializer'

--- app/serializers.py
+++ app/serializers.py
@@ -13,6 +13,7 @@ class StudentSerializer(serializers.ModelSerializer):
         model = Students
         # exclude = ['id', 'name'] Displays all the data excerpt id and name
         fields = '__all__' # Displays all the data
+        # depth = 1 # it displays all the data from rows including id, but serializer gives more control which I did above
     def validate(self, data):
         if data['age'] < 18:
             raise serializers.ValidationError('Age should be above of 18')
